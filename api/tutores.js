d9e28eb73885d950959420752b3618496c4df6cf
Filipe Pacheco <filipe@Filipes-MacBook-Pro.local>
2024-06-08 01:38:49 -0300

    Adicionar cabeçalhos midCORS às funções serverless

--- api/tutores.js
+++ api/tutores.js
@@ -2,15 +2,8 @@ const express = require('express');
 const supabase = require('../db');
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-  next();
-});
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
